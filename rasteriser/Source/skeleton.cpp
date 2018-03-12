a6e3a0d89983bf9fe3e81508dffa167d4327b08f
evridiki91 <ec15441@my.bristol.ac.uk>
2018-03-12 19:56:05 +0000

    Trying to do the homogenous

--- rasteriser/Source/skeleton.cpp
+++ rasteriser/Source/skeleton.cpp
@@ -20,6 +20,15 @@ using glm::mat4;
 
 vec3 white(1,1,1);
 vector<Triangle> triangles;
+
+struct Camera {
+  vec4 position;
+  mat4 R; //rotation
+  float yaw; //angle for rotating around y-axis
+  float pitch;
+  float roll;
+};
+
 /* -------------------------------------------------
 ---------------------------*/
 /* FUNCTIONS                                                                   */
@@ -27,26 +36,67 @@ vector<Triangle> triangles;
 void Update();
 void Draw(screen* screen);
 void TransformationMatrix(glm::mat4x4 M, vec4 translation, vec4 rotation);
-vec4 cameraPos( 0, 0, -3.001,1 );
-void VertexShader( const vec4& v, ivec2& p );
+void VertexShader( const vec4& v, ivec2& p, camera &camera );
 void DrawLineSDL( screen* screen, ivec2 a, ivec2 b, vec3 color );
 void DrawPolygonEdges( const vector<vec4>& vertices,screen* screen );
-// void TransformationMatrix(glm:mat4x4 M, vec4 translation, vec4 rotation){
-//   return
-// }
+void DrawTriangles(vector<Triangle> triangles, screen* screen);
+void TransformationMatrix(glm:mat4x4 M, vec3 translation, vec3 rotation);
 
+void TransformationMatrix(glm:mat4 &M, vec4 translation, mat4 yaw, mat4 roll, mat4 pitch){
+  M = yaw*roll*pitch;
+  M[3] = translation;
+}
+
+//yaw
+void yaw_rotation(Camera& camera, mat4 &R){
+  vec4 v1(cos(camera.yaw), 0, sin(camera.yaw),0);
+  vec4 v2(0,1,0,0);
+  vec4 v3(-sin(camera.yaw), 0, cos(camera.yaw),0);
+  vec4 v4(0,0,0,1);
+  R = mat4(v1,v2,v3,v4);
+}
+
+//roll
+void roll_rotation(Camera& camera, mat4 &R){
+  vec4 v1(cos(camera.roll), -sin(camera.roll), 0 ,0);
+  vec4 v2(sin(camera.roll),cos(camera.roll),0,0);
+  vec4 v3(0, 0, 1,0);
+  vec4 v4(0,0,0,1);
+  R = mat4(v1,v2,v3,v4);
+}
+
+//pitch
+void pitch_rotation(Camera& camera, mat4 &R){
+  vec4 v1(1, 0, 0,0);
+  vec4 v2(0,cos(camera.pitch),-sin(camera.pitch),0);
+  vec4 v3(0, sin(camera.pitch), cos(camera.pitch),0);
+  vec4 v4(0,0,0,1);
+  R = mat4(v1,v2,v3,v4);
+}
+
+void initialize_camera(Camera &camera){
+  camera.position = vec4( 0, 0, -3.001,1 );
+  camera.yaw = 0;
+  camera.pitch = 0;
+  camera.roll = 0;
+  vec4 translation = vec4(0,0,0,0);
+  TransformationMatrix(camera.R, translation, rotation,camera.yaw,camera.pitch,camera.roll );
+}
 
 int main( int argc, char* argv[] )
 {
-
+  Camera camera;
+  initialize_camera(camera);
   screen *screen = InitializeSDL( SCREEN_WIDTH, SCREEN_HEIGHT, FULLSCREEN_MODE );
+  memset(screen->buffer, 0, screen->height*screen->width*sizeof(uint32_t));
   LoadTestModel(triangles);
+  int i = 1;
   while( NoQuitMessageSDL() )
     {
       Update();
       Draw(screen);
       SDL_Renderframe(screen);
-      DrawPolygonEdges(triangles, screen);
+      DrawTriangles(triangles, screen);
     }
 
   SDL_SaveImage( screen, "screenshot.bmp" );
@@ -54,16 +104,21 @@ int main( int argc, char* argv[] )
   KillSDL(screen);
   return 0;
 }
+void DrawTriangles(vector<Triangle> triangles, screen* screen){
+  vector<vec4> vertices;
+  for (uint32_t i = 0; i < triangles.size(); i ++){
+    vertices.push_back(triangles[i].v0);
+    vertices.push_back(triangles[i].v1);
+    vertices.push_back(triangles[i].v2);
+  }
+  DrawPolygonEdges(vertices, screen);
+}
 
 /*Place your drawing here*/
 void Draw(screen* screen)
 {
   /* Clear buffer */
-  memset(screen->buffer, 0, screen->height*screen->width*sizeof(uint32_t));
-
   vec3 colour(1.0,0.0,0.0);
-
-  memset(screen->buffer, 0, screen->height*screen->width*sizeof(uint32_t));
   for( uint32_t i=0; i<triangles.size(); ++i ){
     vector<vec4> vertices(3);
     vertices[0] = triangles[i].v0;
@@ -78,8 +133,8 @@ void Draw(screen* screen)
   }
 }
 
-void VertexShader( const vec4& v, ivec2& p ){
-  vec4 p_origin = v - cameraPos;
+void VertexShader( const vec4& v, ivec2& p, Camera &camera, glm:mat4x4 M ){
+  vec4 p_origin = v - camera.position;
   p.x = FOCAL_LENGTH*p_origin.x/p_origin.z + SCREEN_WIDTH/2;
   p.y = FOCAL_LENGTH*p_origin.y/p_origin.z + SCREEN_HEIGHT/2;
 }
@@ -96,23 +151,22 @@ void Interpolate( ivec2 a, ivec2 b, vector<ivec2>& result ){
 
 //a - start b - end
 void DrawLineSDL( screen* screen, ivec2 a, ivec2 b, vec3 color ){
-  PutPixelSDL(screen, a.x, a.y, white);
-  PutPixelSDL(screen, b.x, b.y, white);
-
   ivec2 delta = glm::abs( a - b );
-  int pixels = glm::max( delta.x, delta.y ) + 1;
+  uint32_t pixels = glm::max( delta.x, delta.y ) + 1;
   vector<ivec2> line( pixels ); //get the pixel positions of the line
   Interpolate( a, b, line );
   for (uint32_t i = 0; i < pixels; i++)
     PutPixelSDL(screen,line[i].x,line[i].y,color);
 }
 
-void DrawPolygonEdges( const vector<vec4>& vertices,screen* screen ) {
+
+
+void DrawPolygonEdges( const vector<vec4>& vertices,screen* screen, Camera &camera ) {
   int V = vertices.size();
   // Transform each vertex from 3D world position to 2D image position:
   vector<ivec2> projectedVertices( V );
   for( int i=0; i<V; ++i ) {
-    VertexShader( vertices[i], projectedVertices[i] );
+    VertexShader( vertices[i], projectedVertices[i],camera );
   }
   // Loop over all vertices and draw the edge from it to the next vertex:
   for( int i=0; i<V; ++i ){
@@ -133,4 +187,41 @@ void Update()
   /*Good idea to remove this*/
   std::cout << "Render time: " << dt << " ms." << std::endl;
   /* Update variables*/
+  int dx;
+  int dy;
+  SDL_GetRelativeMouseState( &dx, &dy );
+
+  // const uint8_t* keystate = SDL_GetKeyboardState(NULL);
+  // if(keystate[SDL_SCANCODE_UP]){
+  //   camera.position.z += SENSITIVITY;
+  // }
+  // else if(keystate[SDL_SCANCODE_DOWN]){
+  //   camera.position.z -= SENSITIVITY;
+  // }
+  // else if(keystate[SDL_SCANCODE_LEFT]){
+  //   rotation_aroundY(camera,1);
+  // }
+  // else if(keystate[SDL_SCANCODE_RIGHT]){
+  //   rotation_aroundY(camera,-1);
+  // }
+  // else if(keystate[SDL_SCANCODE_W]){
+  //   lights[light_selection].pos += forward*LIGHT_SENSITIVITY;
+  // }
+  // else if(keystate[SDL_SCANCODE_S]){
+  //   lights[light_selection].pos -= forward*LIGHT_SENSITIVITY;
+  // }
+  // else if(keystate[SDL_SCANCODE_A]){
+  //     lights[light_selection].pos -= right*LIGHT_SENSITIVITY;
+  // }
+  // else if(keystate[SDL_SCANCODE_D]){
+  //     lights[light_selection].pos += right*LIGHT_SENSITIVITY;
+  // }
+  // else if (keystate[SDL_SCANCODE_I]){
+  //   rotation_aroundX(camera,-1);
+  // }
+  // else if (keystate[SDL_SCANCODE_O]){
+  //   initLights();
+  //   light_selection ++;
+  // }
+
 }
